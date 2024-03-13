ad281c46777dc89b55bf2a9278d57aa71804ceaa
giosteh <stea.giovanni.56@gmail.com>
2024-03-13 17:32:11 +0100

    Finita classe salvataggi

--- AtomicQuest/src/main/java/controller/GestioneSalvataggi.java
+++ AtomicQuest/src/main/java/controller/GestioneSalvataggi.java
@@ -2,15 +2,25 @@
 package controller;
 
 import entita.Giocatore;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.util.List;
+import java.util.ArrayList;
 
 
 public class GestioneSalvataggi {
     
+    private static List<String> nomiSalvataggi = new ArrayList<>();
     private static final String CREAZIONETABELLA = "CREATE TABLE IF NOT EXISTS salvataggi"
             + "(id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(1024), obj BLOB);";
     
@@ -39,15 +49,80 @@ public class GestioneSalvataggi {
         }
     }
     
-    public static void eseguiInsertInDB(final String nome, final Giocatore obj) {
-        
+    public static boolean inserisciInDB(final String nome, final Giocatore obj) {
+        ObjectOutputStream outStream = null;
+        if (GestioneSalvataggi.isNomeDuplicato(nome)) {
+            return false;
+        }
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            outStream = new ObjectOutputStream(baos);
+            outStream.writeObject(obj);
+            byte[] serializedObj = baos.toByteArray();
+            Connection conn = GestioneSalvataggi.connettiAlDB();
+            String insert = "INSERT INTO salvataggi (nome, obj) VALUES (?, ?)";
+            PreparedStatement pstm = conn.prepareStatement(insert);
+            pstm.setString(1, nome);
+            pstm.setBytes(2, serializedObj);
+            pstm.executeUpdate();
+            pstm.close();
+            GestioneSalvataggi.nomiSalvataggi.add(nome);
+            return true;
+        } catch (SQLException | IOException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            try {
+                outStream.close();
+            } catch (IOException ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+        return false;
+    }
+
+    public static Giocatore selezionaDaDB(final String nome) {
+        try {
+            Giocatore obj = null;
+            Connection conn = GestioneSalvataggi.connettiAlDB();
+            String selectPerId = "SELECT obj FROM salvataggi WHERE (nome = ?)";
+            PreparedStatement pstm = conn.prepareStatement(selectPerId);
+            pstm.setString(1, nome);
+            ResultSet res = pstm.executeQuery();
+            if (res.next()) {
+                byte[] serializedObj = res.getBytes(1);
+                ByteArrayInputStream bais = new ByteArrayInputStream(serializedObj);
+                ObjectInputStream inStream = new ObjectInputStream(bais);
+                obj = (Giocatore) inStream.readObject();
+            }
+            pstm.close();
+            return obj;
+        } catch (SQLException | IOException ex) {
+            System.err.println(ex.getMessage());
+        } catch (ClassNotFoundException ex) {
+            System.err.println(ex.getMessage());
+        }
+        return null;
+    }
+
+    public static void rimuoviDaDB(final String nome) {
+        try {
+            Connection conn = GestioneSalvataggi.connettiAlDB();
+            String deletePerNome = "DELETE FROM salvataggi WHERE (nome = ?)";
+            PreparedStatement pstm = conn.prepareStatement(deletePerNome);
+            pstm.setString(1, nome);
+            pstm.executeUpdate();
+            pstm.close();
+            GestioneSalvataggi.nomiSalvataggi.remove(nome);
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+        }
     }
     
-    public static Giocatore eseguiCaricamentoDaDB(final int id) {
-        
+    public static boolean isNomeDuplicato(final String nome) {
+        return GestioneSalvataggi.nomiSalvataggi.contains(nome);
     }
     
-    public static Giocatore eseguiCaricamentoDaDB(final String nome) {
-        
+    public static List<String> getNomiSalvataggi() {
+        return GestioneSalvataggi.nomiSalvataggi;
     }
 }
